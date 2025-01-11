08d3314d255b386ae5a1d882fcbe35dcc8c9d983
ImJiyun <jiyunk920@gmail.com>
2025-01-11 21:25:56 +0900

    upload 22-react-router-advanced

---
+++ 22-react-router-advanced/frontend/src/pages/EventRoot.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import EventsNavigation from "../components/EventsNavigation";
+
+function EventRootLayout() {
+  return (
+    <>
+      <EventsNavigation />
+      <Outlet />
+    </>
+  );
+}
+
+export default EventRootLayout;
